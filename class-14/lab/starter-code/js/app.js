c899b0af1c7b81a7fdd498e9f2e206bd7a7163a8
Waleed A. Afifi <waleedafifich@gmail.com>
2021-08-11 10:59:53 +0300

    class 14 lab

--- class-14/lab/starter-code/js/app.js
+++ class-14/lab/starter-code/js/app.js
@@ -8,15 +8,19 @@ const Cart = function(items) {
 
 Cart.prototype.addItem = function(product, quantity) {
   // TODO: Fill in this instance method to create a new CartItem and add it to this.items
+  let cartItemObj = new CartItem(product, quantity);
+  this.items.push(cartItemObj);
 };
 
 Cart.prototype.saveToLocalStorage = function() {
   // TODO: Fill in this instance method to save the contents of the cart to localStorage
+  localStorage.cart = JSON.stringify(this.items);
 };
 
 Cart.prototype.removeItem = function(item) {
   // TODO: Fill in this instance method to remove one item from the cart.
   // Note: You will have to decide what kind of parameter to pass in here!
+  this.items.splice(item, 1);
 };
 
 const CartItem = function(product, quantity) {
@@ -56,3 +60,5 @@ function generateCatalog() {
 
 // Initialize the app by creating the big list of products with images and names
 generateCatalog();
+
+console.log(Product.allProducts)
